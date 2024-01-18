723c4912c5574318b98893d34f1c676db15f47e3
HimalN <himalnimsara888@gmail.com>
2024-01-18 20:17:58 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/LauncherWrapper.java
@@ -0,0 +1,13 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LauncherWrapper  {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
